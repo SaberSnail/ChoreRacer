336bc34f23b59480aa701eb89f760968c074e463
abatishko <andrew@batishko.net>
2018-01-14 16:09:53 -0800

    Work on displaying cars

--- Client/src/App.js
+++ Client/src/App.js
@@ -6,9 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Car color="#ff0000" />
-        </header>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 800">
+          <Car color="pink" size="40" x="0" y="-200" />
+          <Car color="red" size="40" x="10" y="-100" />
+          <Car color="green" size="40" x="20" y="0" />
+          <Car color="blue" size="40" x="30" y="100" />
+          <Car color="orange" size="40" x="50" y="200" />
+        </svg>
       </div>
     );
   }
